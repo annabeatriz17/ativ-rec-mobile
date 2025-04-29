b6caf0cf143a6efc59af91bda5786ec13f453789
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-29 13:29:57 -0300

    update

---
+++ my-app/pages/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native-web";
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <Text>Movies</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        textAlign: "center",
+        backgroundColor: "#000000",
+    },
+});
